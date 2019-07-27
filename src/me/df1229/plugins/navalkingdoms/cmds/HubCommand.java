c3a1518de24170ac3e974c4475970f4b2fb72b92
DF1229 <daanfaber1@gmail.com>
2019-07-27 21:45:37 +0200

    Support for chat-prefix config option

--- src/me/df1229/plugins/navalkingdoms/cmds/HubCommand.java
+++ src/me/df1229/plugins/navalkingdoms/cmds/HubCommand.java
@@ -5,21 +5,31 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+import me.df1229.plugins.navalkingdoms.NavalKingdoms;
 
 public class HubCommand implements CommandExecutor {
 	
 	private static ClaimRegion claimExec = new ClaimRegion();
 	private static UnclaimRegion unclaimExec = new UnclaimRegion();
+	
+	private Plugin plugin = NavalKingdoms.getPlugin(NavalKingdoms.class);
+	private String prefix = plugin.getConfig().getString("chat-prefix");
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		if (prefix == null) {
+			sender.sendMessage(ChatColor.RED + "Internal Plugin Error: Cannot load plugin's chat prefix, is it in config.yml?");
+		}
 		
 		if (args.length != 1) {
 			return false;
 		}
 		
 		if (!(sender instanceof Player)) {
-			sender.sendMessage(ChatColor.RED + "[NavalKingdoms] You can't use that command here!");
+			sender.sendMessage(ChatColor.RED + prefix + " You can't use that command here!");
 			return true;
 		}
 		
@@ -33,12 +43,26 @@ public class HubCommand implements CommandExecutor {
 		case "unclaim":
 			unclaimExec.executeCmd(sender, cmd, label, args);
 			break;
+		case "reload":
+			reloadConfig(player);
+			break;			
 		default:
-			player.sendMessage(ChatColor.RED + "[NavalKingdoms] Command error: could not parse command, please notify staff");
+			player.sendMessage(ChatColor.RED + prefix + " Command error: could not parse command, please notify staff");
 			break;
 		}
 		
 		return true;
+	}
+	
+	private void reloadConfig(Player player) {
+		
+		if (!player.hasPermission("navalkingdoms.reload")) {
+			player.sendMessage(ChatColor.RED + prefix + " You don't have permission to use that command.");
+			return;
+		}
+		
+		plugin.reloadConfig();
+		player.sendMessage(ChatColor.GREEN + prefix + " Config.yml reloaded");
 	}
 
 }
