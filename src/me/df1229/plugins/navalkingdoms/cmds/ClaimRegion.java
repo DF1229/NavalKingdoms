25cba218e7ad80e88a0c2cf902f66829df2f4ca6
DF1229 <daanfaber1@gmail.com>
2019-08-02 12:02:58 +0200

    Code cleanuo, preparation for future features

--- src/me/df1229/plugins/navalkingdoms/cmds/ClaimRegion.java
+++ src/me/df1229/plugins/navalkingdoms/cmds/ClaimRegion.java
@@ -28,9 +28,11 @@ public class ClaimRegion {
 	
 	private static Plugin plugin = NavalKingdoms.getPlugin(NavalKingdoms.class);
 
-	public boolean executeCmd(CommandSender sender, Command command, String label, String[] args) {
+	public void executeCmd(CommandSender sender, Command command, String label, String[] args) {
 		
 		String prefix = plugin.getConfig().getString("chat-prefix");
+		Boolean checkOverlap = plugin.getConfig().getBoolean("prevent-overlapping-claims");
+		Boolean markBoundary = plugin.getConfig().getBoolean("mark-claim-boundary");
 		
 		if (prefix == null) {
 			sender.sendMessage(ChatColor.RED + "Internal Plugin Error: Cannot load plugin's chat prefix, is it in config.yml?");
@@ -45,12 +47,12 @@ public class ClaimRegion {
 		WorldGuardPlugin wgPlugin = WGBukkit.getPlugin();
 		if (!(wgPlugin instanceof WorldGuardPlugin)) {
 			sender.sendMessage(ChatColor.RED + prefix + " Error: Unable to load WorldGuard");
-			return true;
+			return;
 		}
 		
 		if (!(sender instanceof Player)) {
 			sender.sendMessage(ChatColor.RED + prefix + " You can only register a claim as a player");
-			return true;
+			return;
 		}
 		
 		Player player = (Player) sender; // Player object of sender
@@ -59,14 +61,14 @@ public class ClaimRegion {
 		
 		if (container == null) {
 			player.sendMessage(ChatColor.RED + prefix + " Error: Could not load WorldGuard data, please contact the server's staff!");
-			return true;
+			return;
 		}
 		
 		RegionManager regions = container.get(player.getWorld()); // RegionManager object for all regions in the player's world.
 		
 		if (regions == null) {
 			player.sendMessage(ChatColor.RED + prefix + " Error: Could not load " + player.getWorld().getName() + "'s regions, please contact the server's staff!");
-			return true;
+			return;
 		}
 		
 		String playerName = player.getName(); // Player's name
@@ -85,7 +87,9 @@ public class ClaimRegion {
 			
 			ProtectedRegion newRegion = new ProtectedCuboidRegion(playerName, pos1, pos2); // Definition of the new region 
 			
-			// TODO: Add overlap checking
+			if (checkOverlap) {
+				// TODO: Add overlap checking
+			}
 			
 			DefaultDomain owners = newRegion.getOwners(); // List of current owners of the region
 			owners.addPlayer(playerUUID);
@@ -98,15 +102,17 @@ public class ClaimRegion {
 			
 			regions.addRegion(newRegion); // New region gets added to the regions of that world
 			
-			// TODO: Add feature support for territorial mark above the region
+			if (markBoundary) {
+				// TODO: Add feature support for territorial mark above the region
+			}
 			
 			player.sendMessage(ChatColor.GREEN + prefix + " Your region has been created!");
 				
-			return true;
+			return;
 		} else {
 			// Region already exists
 			player.sendMessage(ChatColor.AQUA + prefix + " You already have a claim, use " + ChatColor.ITALIC + "/nk unclaim " + ChatColor.RESET + ChatColor.AQUA + "to remove your region.");
-			return true;
+			return;
 		}
 		
 	}
